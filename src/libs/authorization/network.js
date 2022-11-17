2846d84b373cd73cea780318ea434a83524d5643
mephistopheles1010 <47539531+mephistopheles1010@users.noreply.github.com>
2022-11-17 19:00:35 +0200

    refactor(authorization): make protected class members private

--- src/libs/authorization/network.js
+++ src/libs/authorization/network.js
@@ -3,15 +3,18 @@ export class AuthorizationNetwork {
     return new AuthorizationNetwork(authorizationService, errorComposer);
   }
 
+  #authorizationService;
+  #composeError;
+
   constructor(authorizationService, errorComposer) {
-    this._authorizationService = authorizationService;
-    this._composeError = errorComposer;
+    this.#authorizationService = authorizationService;
+    this.#composeError = errorComposer;
   }
 
   async onVerifyAccess(req, res, next) {
-    const isAuthorized = await this._authorizationService.isAuthorized(req.user?.id);
+    const isAuthorized = await this.#authorizationService.isAuthorized(req.user?.id);
     if (!isAuthorized) {
-      return res.status(401).send(this._composeError(401, 'Unauthorized'));
+      return res.status(401).send(this.#composeError(401, 'Unauthorized'));
     }
     return next();
   }
