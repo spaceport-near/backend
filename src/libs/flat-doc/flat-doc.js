f6f186f81c38aa7b6dd53bd1d6d1aaad35ec9fa9
mephistopheles1010 <47539531+mephistopheles1010@users.noreply.github.com>
2022-11-17 19:00:54 +0200

    refactor(FlatDoc): make protected class members private

--- src/libs/flat-doc/flat-doc.js
+++ src/libs/flat-doc/flat-doc.js
@@ -2,32 +2,32 @@ export class FlatDoc {
   static makeFlat(doc, stopper) {
     const flatDoc = {};
     stopper = (typeof stopper === 'function') ? stopper : () => false;
-    FlatDoc.flat(flatDoc, '', doc, stopper);
+    FlatDoc.#flat(flatDoc, '', doc, stopper);
     return flatDoc;
   }
 
-  static flat(flatDoc, flatKey, doc, stopper) {
+  static #flat(flatDoc, flatKey, doc, stopper) {
     Object.entries(doc).forEach(([key, value]) => {
-      const updatedFlatKey = FlatDoc.updateFlatKey(flatKey, key);
+      const updatedFlatKey = FlatDoc.#updateFlatKey(flatKey, key);
       if (stopper(doc, key)) {
-        FlatDoc.appendFlatKey(flatDoc, flatKey, {[key]: value});
-      } else if (FlatDoc.canDeeper(value)) {
-        FlatDoc.flat(flatDoc, updatedFlatKey, value, stopper);
+        FlatDoc.#appendFlatKey(flatDoc, flatKey, {[key]: value});
+      } else if (FlatDoc.#canDeeper(value)) {
+        FlatDoc.#flat(flatDoc, updatedFlatKey, value, stopper);
       } else {
-        FlatDoc.appendFlatKey(flatDoc, updatedFlatKey, value);
+        FlatDoc.#appendFlatKey(flatDoc, updatedFlatKey, value);
       }
     });
   }
 
-  static canDeeper(value) {
+  static #canDeeper(value) {
     return !!(value && typeof value === 'object' && Object.keys(value).length && !Array.isArray(value));
   }
 
-  static updateFlatKey(flatKey, key) {
+  static #updateFlatKey(flatKey, key) {
     return flatKey ? `${flatKey}.${key}` : `${key}`;
   }
 
-  static appendFlatKey(flatDoc, flatKey, value) {
+  static #appendFlatKey(flatDoc, flatKey, value) {
     flatDoc[flatKey] = value;
   }
 }
